55abc3b4f6a0de1d142ec9de4b9722016f74cddf
valentinolm <vlmvalenlozanomassey@gmail.com>
2025-11-17 10:16:36 -0300

    funciones mostrar ocultar de users

--- js/user.js
+++ js/user.js
@@ -412,3 +412,19 @@ function initUserPanel() {
 
 // arrancar
 initUserPanel();
+
+// funciones mostrar/ocultar
+function cerrarSesion() {
+    localStorage.removeItem("usuarioLogueado");
+    window.location.href = "index.html";
+}
+
+function mostrar(id) {
+    document.querySelectorAll(".card").forEach(s => s.style.display = "none");
+    document.getElementById(id).style.display = "block";
+}
+
+function volverMenu() {
+    document.querySelectorAll(".card").forEach(s => s.style.display = "none");
+    document.getElementById("menuPrincipal").style.display = "block";
+}
